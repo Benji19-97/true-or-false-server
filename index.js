4f6c7fb809bcf3ca8528ed97be004074d64d9f26
Benji <benjamin.bruckmoser@web.de>
2021-11-06 20:30:33 +0100

    cors stuff

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+var allowCrossDomain = function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+
+      // intercept OPTIONS method
+      if ("OPTIONS" == req.method) {
+            res.send(200);
+      } else {
+            next();
+      }
+};
+
+app.use(allowCrossDomain);
+
 app.use(express.json());
 
 app.listen(process.env.PORT || 3000, () => console.log("Server started..."));
