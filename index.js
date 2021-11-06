b4461ed56ff425a2296723d9c84f633b207e68a5
Benji <benjamin.bruckmoser@web.de>
2021-11-06 20:24:15 +0100

    changed line

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+
 app.listen(process.env.PORT || 3000, () => console.log("Server started..."));
 
 const menu = require("./routes/testroute");
