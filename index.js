d7808a099f4ab985b98f807238caadec202fdfac
Benji <benjamin.bruckmoser@web.de>
2021-11-06 20:28:28 +0100

    remove code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+
 app.use(express.json());
 
 app.listen(process.env.PORT || 3000, () => console.log("Server started..."));
